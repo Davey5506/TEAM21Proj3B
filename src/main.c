ff85e2d3751feca4bd31001bd284322c95f4da22
David <kpdmmvi@gmail.com>
2025-10-14 20:13:03 -0400

    Cleared TIM3_IRQHandler

--- src/main.c
+++ src/main.c
@@ -1,22 +1,6 @@
 #include "hat.h"
 uint16_t pulse_duration = 0;
 void TIM3_IRQHandler(void){
-    if(TIM3->SR & TIM_SR_UIF){
-        TIM3->SR &= ~TIM_SR_UIF;
-        // Send 10us pulse on TRIG pin
-        toggle_pin(ULTRA_SOUND.TRIG_PORT, ULTRA_SOUND.TRIG_PIN);
-        uint16_t current_time = TIM3->CNT;
-        while(TIM3->CNT - current_time < 10);
-        toggle_pin(ULTRA_SOUND.TRIG_PORT, ULTRA_SOUND.TRIG_PIN);
-
-        // Wait for ECHO pin to go high
-        while(!read_pin(ULTRA_SOUND.ECHO_PORT, ULTRA_SOUND.ECHO_PIN));
-        uint16_t start_time = TIM3->CNT;
-        // Wait for ECHO pin to go low
-        while(read_pin(ULTRA_SOUND.ECHO_PORT, ULTRA_SOUND.ECHO_PIN));
-        pulse_duration = TIM3->CNT - start_time;
-        display_num(pulse_duration / 58, 2); // Convert to cm and display
-    }
 }
 
 int main() {
