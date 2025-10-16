d697d8441ac006482810174ef528e9d5434dab4a
David <kpdmmvi@gmail.com>
2025-10-16 01:34:10 -0400

    Removed optional timer enable

--- lib/hat/hat.c
+++ lib/hat/hat.c
@@ -188,9 +188,7 @@ void init_gp_timer(TIM_TypeDef* TIMx, uint32_t freq, uint32_t arr){
     TIMx->PSC = (SYSTEM_FREQ / freq) - 1;
     TIMx->ARR = arr;
     TIMx->CNT = 0;
-    if(enable){
         TIMx->CR1 |= TIM_CR1_CEN;
-    }
     return;
 }
 
